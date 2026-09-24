34d8f8b8ca7a88fdba05ec8279671bf159b773dc
rudersingh <2024a1r040@mietjammu.in>
2026-09-24 12:12:24 +0530

    Update task51.py

--- task51.py
+++ task51.py
@@ -1,7 +1,21 @@
-n = int(input("Enter number of elements: "))
-a = list(map(int, input("Enter numbers: ").split()))
+# Write a program to input numbers in a list and find the second largest number. 
 
-a = list(set(a))
-a.sort()
+n = int(input("Enter the number of enteries: "))
 
-print("Second largest number:", a[-2])
+list = []
+
+for i in range(n):
+    m = float(input(f"Enter the {i + 1}st entry: "))
+    list.append(m)
+
+largest = list[0]
+second_largest = list[0]
+
+for i in range(1, n):
+    if list[i] > largest:
+        second_largest = largest
+        largest = list[i]
+    elif list[i] > second_largest and list[i] != largest:
+        second_largest = list[i]
+
+print("The second largest number is:", second_largest)
